0ee5f45bf0f9097b6efe7b1cca7e32cfdef37a31
Richard Gross <richard.gross@maibornwolff.de>
2019-02-11 14:40:41 +0100

    add application context test

---
+++ price/src/test/kotlin/de/richargh/springkotlinhexagonal/ApplicationContextTest.kt
@@ -0,0 +1,42 @@
+package de.richargh.springkotlinhexagonal
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.context.support.GenericApplicationContext
+
+class ApplicationContextTest {
+
+    @Test
+    fun `application context can resolve the greeter`() {
+        // arrange
+        val applicationContext = GenericApplicationContext().apply {
+            homeBeans().initialize(this)
+            refresh()
+        }
+
+        // act
+        val foo = applicationContext.getBean(Foo::class.java)
+        val greeting = foo.sayHello()
+
+        // assert
+        assertThat(greeting).isEqualTo("Hello")
+    }
+
+    @Test
+    fun `application context can resolve the mock greeter`() {
+        // arrange
+        val applicationContext = GenericApplicationContext().apply {
+            homeBeans().initialize(this)
+            testHomeBeans().initialize(this)
+            refresh()
+        }
+
+        // act
+        val foo = applicationContext.getBean(Foo::class.java)
+        val greeting = foo.sayHello()
+
+        // assert
+        assertThat(greeting).isEqualTo("Moin")
+    }
+
+}
