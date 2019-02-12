526a4b376623021552591142d99cf22d798048ba
Richard Gross <richard.gross@maibornwolff.de>
2019-02-12 16:14:22 +0100

    add more bean placeholders

--- price/src/main/kotlin/de/richargh/springkotlinhexagonal/HomeBeans.kt
+++ price/src/main/kotlin/de/richargh/springkotlinhexagonal/HomeBeans.kt
@@ -1,8 +1,20 @@
 package de.richargh.springkotlinhexagonal
 
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.support.beans
 
 fun homeBeans() = beans {
     bean<Greeter>()
     bean<HomeController>()
+    environment({!activeProfiles.contains("foo")}){
+        bean {
+
+        }
+    }
+
+    profile("foo") {
+        bean { }
+    }
 }
