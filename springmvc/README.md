b40928bc9d7ac2b6e5c3a7a496ab82b6e457564a
Richard Gross <richard.gross@maibornwolff.de>
2019-02-12 22:20:43 +0100

    add Pom, Readme and simple string replacement

---
+++ springmvc/README.md
@@ -0,0 +1,10 @@
+# Spring Mvc Service
+
+Uses only the Spring framework and not Spring Boot.
+
+Based on this tutorial: https://github.com/eugenp/tutorials/tree/master/spring-mvc-kotlin 
+and this: https://github.com/fernandospr/spring-jetty-example/blob/master/src/main/java/com/fernandospr/example/server/EmbeddedJetty.java
+
+## Performance
+
+With 
--- springmvc/src/main/kotlin/de/richargh/springkotlinhexagonal/HomeController.kt
+++ springmvc/src/main/kotlin/de/richargh/springkotlinhexagonal/HomeController.kt
@@ -2,12 +2,15 @@ package de.richargh.springkotlinhexagonal
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class HomeController{
 
     @RequestMapping("/")
-    fun home(): String{
-        return "Home"
+    fun home(): ModelAndView{
+        val mav = ModelAndView("Home")
+        mav.addObject("name", "Foo")
+        return mav
     }
 }
