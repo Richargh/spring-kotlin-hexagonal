ba3bc398f8845d170ca868c891a1e1d32732981f
Richard Gross <richard.gross@maibornwolff.de>
2019-05-04 14:44:49 +0200

    removed functional routes because they were not recognized any more

--- price/src/main/kotlin/de/richargh/springkotlinhexagonal/config/FunctionalHomeConfig.kt
+++ price/src/main/kotlin/de/richargh/springkotlinhexagonal/config/FunctionalHomeConfig.kt
@@ -2,24 +2,11 @@ package de.richargh.springkotlinhexagonal.config
 
 import de.richargh.springkotlinhexagonal.web.HomeController
 import de.richargh.springkotlinhexagonal.domain.Replier
-import de.richargh.springkotlinhexagonal.web.FunctionalHomeRoutes
 import org.springframework.context.support.beans
-import org.springframework.web.reactive.function.server.HandlerStrategies
-import org.springframework.web.reactive.function.server.RouterFunctions
-import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver
 
 fun functionalHomeConfig() = beans {
     bean<Replier>()
     bean<HomeController>()
-    bean<FunctionalHomeRoutes>()
-    bean("webHandler") {
-        RouterFunctions.toWebHandler(ref<FunctionalHomeRoutes>().router(), HandlerStrategies.builder().viewResolver(ref()).build())
-    }
-
-    bean {
-        val prefix = "classpath:/templates/"
-        ThymeleafReactiveViewResolver()
-    }
 
     environment({!activeProfiles.contains("foo")}){
         bean {
--- price/src/main/kotlin/de/richargh/springkotlinhexagonal/web/HomeController.kt
+++ price/src/main/kotlin/de/richargh/springkotlinhexagonal/web/HomeController.kt
@@ -12,21 +12,13 @@ class HomeController(private val greeter: Greeter, private val replier: Replier)
 
     @GetMapping
     fun index(): ModelAndView {
-        return createGreeting()
-    }
-
-    @GetMapping("reply")
-    fun reply(@RequestParam("name") name: String): ModelAndView {
-        return createReply(name)
-    }
-
-    private fun createGreeting(): ModelAndView {
         val mav = ModelAndView("greeting")
         mav.addObject("message", greeter.sayHello())
         return mav
     }
 
-    private fun createReply(name: String): ModelAndView {
+    @GetMapping("reply")
+    fun reply(@RequestParam("name") name: String): ModelAndView {
         val mav = ModelAndView("reply")
         mav.addObject("message", replier.speak(name))
         return mav
